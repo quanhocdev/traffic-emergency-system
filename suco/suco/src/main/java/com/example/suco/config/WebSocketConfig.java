5628c3adc7661ff93e7c5b1a4903813fa70b30a4
Huỳnh Minh Quân <minhquanthcs257@gmail.com>
2026-07-02 12:18:51 +0700

    issue #255: Fix bug socket sự cố

--- suco/suco/src/main/java/com/example/suco/config/WebSocketConfig.java
+++ suco/suco/src/main/java/com/example/suco/config/WebSocketConfig.java
@@ -1,47 +1,47 @@
-package com.example.suco.config;
+    package com.example.suco.config;
 
-import com.example.suco.security.WebSocketAuthInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.web.socket.config.annotation.*;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+    import com.example.suco.security.WebSocketAuthInterceptor;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.messaging.simp.config.ChannelRegistration;
+    import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+    import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+    import org.springframework.web.socket.config.annotation.*;
+    import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Configuration
+    @EnableWebSocketMessageBroker
+    public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final WebSocketAuthInterceptor webSocketAuthInterceptor;
+        private final WebSocketAuthInterceptor webSocketAuthInterceptor;
 
-    public WebSocketConfig(WebSocketAuthInterceptor webSocketAuthInterceptor) {
-        this.webSocketAuthInterceptor = webSocketAuthInterceptor;
+        public WebSocketConfig(WebSocketAuthInterceptor webSocketAuthInterceptor) {
+            this.webSocketAuthInterceptor = webSocketAuthInterceptor;
+        }
+
+        @Override
+        public void configureMessageBroker(MessageBrokerRegistry config) {
+
+            config.enableSimpleBroker("/topic", "/queue");
+
+            config.setApplicationDestinationPrefixes("/app");
+            config.setUserDestinationPrefix("/user");
+        }
+
+        @Override
+        public void registerStompEndpoints(StompEndpointRegistry registry) {
+            // Endpoint cho mobile (App Android / iOS)
+            registry.addEndpoint("/ws-suco")
+                    .setAllowedOriginPatterns("*");
+
+            // Endpoint cho Web Trụ sở / Admin
+            registry.addEndpoint("/ws-suco-web")
+                    .setAllowedOriginPatterns("*")
+                    .addInterceptors(new HttpSessionHandshakeInterceptor()) // 🌟 Bắt Cookie đưa vào Session Attributes
+                    .withSockJS();
+        }
+
+        @Override
+        public void configureClientInboundChannel(ChannelRegistration registration) {
+            registration.interceptors(webSocketAuthInterceptor);
+        }
     }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-
-        config.enableSimpleBroker("/topic", "/queue");
-
-        config.setApplicationDestinationPrefixes("/app");
-        config.setUserDestinationPrefix("/user");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // Endpoint cho mobile (App Android / iOS)
-        registry.addEndpoint("/ws-suco")
-                .setAllowedOriginPatterns("*");
-
-        // Endpoint cho Web Trụ sở / Admin
-        registry.addEndpoint("/ws-suco-web")
-                .setAllowedOriginPatterns("*")
-                .addInterceptors(new HttpSessionHandshakeInterceptor()) // 🌟 Bắt Cookie đưa vào Session Attributes
-                .withSockJS();
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(webSocketAuthInterceptor);
-    }
-}
